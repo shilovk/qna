de6055917c529c8cd995acf76ac4a2f30a1e11e9
ShilovK <shilovk@gmail.com>
2020-03-15 16:24:57 +0300

    Answer model with specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_123609) do
+ActiveRecord::Schema.define(version: 2020_03_15_132021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", id: :serial, force: :cascade do |t|
     t.string "title", limit: 25, null: false
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,14 +3,4 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  it 'validates presence of title' do
-    question = Question.new(body: '123')
-    expect(question).to_not be_valid
-  end
-
-  it 'validates presence of body' do
-    question = Question.new(title: '123')
-    expect(question).to_not be_valid
-  end
 end
