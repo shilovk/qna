182db4794667c0271550e6e8bea04b1dfe9d654f
ShilovK <shilovk@gmail.com>
2020-04-06 02:14:17 +0300

    fix

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,7 +2,7 @@
 
 module LinksHelper
   def parsed_url(url, name = nil)
-    if url.match(%r{^(http|https)?://gist.github.com})
+    if url.=~(%r{^(http|https)?://gist.github.com})
       "<script src=\'#{url}.js\'></script>".html_safe
     else
       link_to(name || url, url)
