001a6b4ae450788599095a48f396034781df7e42
ShilovK <shilovk@gmail.com>
2020-04-10 18:39:47 +0300

    Implementation find_for_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,27 @@ class User < ApplicationRecord
   has_many :answers
   has_many :awards, dependent: :destroy
   has_many :votes
+  has_many :authorizations
 
-  def self.find_for_oauth(data); end
+  def self.find_for_oauth(auth)
+    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.find_by(email: email)
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+    end
+    user.create_authorization(auth)
+    user
+  end
 
   def author?(record)
     id == record&.user_id
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_191007) do
+ActiveRecord::Schema.define(version: 2020_04_10_144234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2020_04_06_191007) do
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
+  end
+
   create_table "awards", force: :cascade do |t|
     t.string "title", null: false
     t.bigint "question_id", null: false
@@ -59,6 +68,17 @@ ActiveRecord::Schema.define(version: 2020_04_06_191007) do
 
   create_table "categories", id: :serial, force: :cascade do |t|
     t.string "title", limit: 25, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "links", force: :cascade do |t|
@@ -111,8 +131,10 @@ ActiveRecord::Schema.define(version: 2020_04_06_191007) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "awards", "questions"
   add_foreign_key "awards", "users"
+  add_foreign_key "comments", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "votes", "users"
 end
