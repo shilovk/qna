617dd42538012e34cb076c1d0e9bd06989f55ab5
ShilovK <shilovk@gmail.com>
2020-04-11 13:42:17 +0300

    Remove concern oauth

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class OauthCallbacksController < Devise::OmniauthCallbacksController
-  include Oauth
+  before_action :oauth
 
   Devise.omniauth_configs.each_key { |service| define_method(service) {} }
+
+  private
+
+  def oauth
+    user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if user&.persisted?
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: action_name) if is_navigational_format?
+    else
+      redirect_to root_path, alert: 'Invalid credentials'
+    end
+  end
 end
