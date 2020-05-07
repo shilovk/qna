1456c1ef6cd353ccc633afdafd54aaca00779a00
ShilovK <shilovk@gmail.com>
2020-05-07 22:09:41 +0300

    Fragmented Caching for resources

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should belong_to(:question) }
+  it { should belong_to(:question).touch(true) }
   it { should belong_to(:user) }
   it { should have_many(:links).dependent(:destroy) }
   it { should have_many(:votes).dependent(:destroy) }
